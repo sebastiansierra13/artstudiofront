ecbda34f0c277f990fac189542a285ff0861f9f8
sebastiansierra13 <johan.sierra@uptc.edu.co>
2024-10-13 21:02:10 -0500

    Version produccion, cambios en blogs y logo

--- src/app/components/blog-detail/blog-detail.component.ts
+++ src/app/components/blog-detail/blog-detail.component.ts
@@ -5,13 +5,14 @@ import { CommonModule } from '@angular/common';
 import { FloatingButtonsComponent } from "../floating-buttons/floating-buttons.component";
 import { FooterComponent } from "../footer/footer.component";
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router'; // Asegúrate de tener esta importación
 
 @Component({
     selector: 'app-blog-detail',
     standalone: true,
     templateUrl: './blog-detail.component.html',
     styleUrls: ['./blog-detail.component.css'],
-    imports: [CommonModule, NavBarComponent, FloatingButtonsComponent, FooterComponent]
+    imports: [CommonModule, NavBarComponent, FloatingButtonsComponent, FooterComponent, RouterModule]
 })
 export class BlogDetailComponent implements OnInit {
   blog: any;
@@ -20,15 +21,21 @@ export class BlogDetailComponent implements OnInit {
   constructor(private route: ActivatedRoute, private blogService: BlogService) {}
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.blogService.getBlogById(id).subscribe(data => {
-        this.blog = data;
-      });
-    }
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      if (id) {
+        this.loadBlog(id);
+      }
+    });
 
     this.blogService.getBlogs().subscribe(data => {
       this.recentPosts = data.slice(0, 5);
+    });
+  }
+
+  loadBlog(id: string) {
+    this.blogService.getBlogById(id).subscribe(data => {
+      this.blog = data;
     });
   }
 }
